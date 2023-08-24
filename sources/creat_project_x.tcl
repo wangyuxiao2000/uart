a25629d0b081563585d2a20b13647e0e723d91d1
WangYuxiao <wyxee2000@163.com>
2023-08-24 18:46:52 +0800

    升级creat_project_x.tcl

--- sources/creat_project_x.tcl
+++ sources/creat_project_x.tcl
@@ -1,38 +1,37 @@
-#设置工程名(将[]替换为工程名)
+# 设置工程名(将[]替换为工程名)
 set project_name UART
-#设置芯片型号(将[]替换为芯片型号)
+# 设置芯片型号(将[]替换为芯片型号)
 set chip_type xc7a35tcsg324-1
-#是否将当前设计导出为自定义IP核(ip_out_p=1进行)
+# 是否将当前设计导出为自定义IP核(ip_out_p=1进行)
 set ip_out_p 1
-#是否进行布局布线、生成烧录比特流的操作(bitstream_p=1进行)
+# 是否进行综合操作(synth_p=1进行)
+set synth_p 0
+# 是否进行布局布线、生成烧录比特流的操作(bitstream_p=1进行)
 set bitstream_p 0
 #是否在所有操作结束后关闭工程(close_p=1关闭)
 set close_p 1
 
-#规定线程数(提升线程数可以使运行更快)
+# 规定线程数(提升线程数可以使运行更快)
 set_param general.maxThreads 8
-#新建outputs文件夹,存储 RTL视图、比特流文件、综合报告等输出
+# 新建outputs文件夹,存储 RTL视图、比特流文件、综合报告等输出
 file delete -force ./outputs
 file mkdir ./outputs
-#创建工程
+# 创建工程
 create_project -part $chip_type -force $project_name    
-#添加RTL设计文件至sources_1文件集
+# 添加RTL设计文件至sources_1文件集
 add_files -fileset sources_1 -norecurse -scan_for_includes ../sources/RTL
-#添加测试文件至sim_1文件集
+# 添加测试文件至sim_1文件集
 add_files -fileset sim_1 -norecurse -scan_for_includes ../sources/TB
-#更新编译顺序(形成模块间的层次关系,找到顶层模块)
+# 更新编译顺序(形成模块间的层次关系,找到顶层模块)
 update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
-#生成RTL视图并导出为PDF
+# 生成RTL视图并导出为PDF
 synth_design -rtl -rtl_skip_mlo -name rtl_1
 write_schematic -format pdf -orientation portrait ./schematic.pdf
 file copy -force ./schematic.pdf ./outputs/schematic.pdf
 file delete -force ./schematic.pdf
-#进行综合
-launch_runs synth_1 -jobs 12
-wait_on_run synth_1
 
-#将当前设计导出为IP(可在46行处添加IP核的相关描述)
+# 将当前设计导出为IP(可在46行处添加IP核的相关描述)
 if {$ip_out_p=="1"} { 
 file delete -force ../my_ip/$project_name
 file mkdir ../my_ip/$project_name
@@ -91,7 +90,13 @@ ipx::move_temp_component_back -component [ipx::current_core]
 close_project -delete
 }
 
-#布局布线、生成烧录比特流(若不使用wait_on_run命令,布局布线还没执行完就会执行生成比特流的命令,从而导致错误)
+# 综合
+if {$synth_p=="1"} {
+launch_runs synth_1 -jobs 12
+wait_on_run synth_1
+}
+
+# 布局布线、生成烧录比特流(若不使用wait_on_run命令,布局布线还没执行完就会执行生成比特流的命令,从而导致错误)
 if {$bitstream_p=="1"} { 
 launch_runs impl_1 -jobs 12
 wait_on_run impl_1
@@ -101,7 +106,7 @@ file copy -force ./$project_name.runs/impl_1/$project_name.bit ./outputs/$projec
 file delete -force ./$project_name.runs/impl_1/$project_name.bit
 }
 
-#结束后关闭工程
+# 结束后关闭工程
 if {$close_p=="1"} { 
 close_project
 stop_gui

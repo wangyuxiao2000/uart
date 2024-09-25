dfbae0222905d61d139417994e47cad4f0946518
WangYuxiao <wyxee2000@163.com>
2024-09-25 14:26:39 +0800

    修改仿真时间精度

--- sources/TB/UART_tb.v
+++ sources/TB/UART_tb.v
@@ -5,7 +5,7 @@
 //Data    : 2022.9.1
 //Version : V 1.1
 /*************************************************************/
-`timescale 1 ns / 1 ns       /*定义 仿真时间单位/精度*/
+`timescale 1 ns / 1 ps       /*定义 仿真时间单位/精度*/
 `define Period 20            /*定义 时钟周期*/
 
 `define stimulus_path "../sources/TB/stimulus.txt" /*定义 激励文件路径*/
